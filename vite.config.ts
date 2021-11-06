44a04c585844dee6fca795650e14521866a6ef3b
hwb <313200064@qq.com>
2021-11-06 21:17:29 +0800

    feat: init

---
+++ vite.config.ts
@@ -0,0 +1,21 @@
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import { resolve } from "path";
+
+const pathResolve = path => resolve(__dirname, path)
+
+// https://vitejs.cn/config/
+export default defineConfig({
+  plugins: [vue()],
+  resolve:{
+    alias: {
+      '@': pathResolve('src'),
+      'comps': pathResolve('src/components'),
+      'apis': pathResolve('src/apis'),
+      'views': pathResolve('src/views'),
+      'utils': pathResolve('src/utils'),
+      'routes': pathResolve('src/routes'),
+      'styles': pathResolve('src/styles'),
+    }
+  }
+})
