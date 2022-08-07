6971a09e5bf7ae21e8177d4bb8f874450a01b094
hwb <313200064@qq.com>
2022-08-07 13:29:19 +0800

    feat: 基础布局

---
+++ src/layouts/default/index.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="app-wrap">
+    <!-- 侧边栏 -->
+    <div class="sidebar-container"></div>
+    <!-- 右侧内容区 -->
+    <div class="main-container">
+      <!-- 顶部导航栏 -->
+      <div class="navbar-wrap"></div>
+      <!-- 内容区 -->
+      <main>111</main>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts"></script>
+
+<style></style>
--- src/router/index.ts
+++ src/router/index.ts
@@ -4,13 +4,19 @@ const router = createRouter({
   history: createWebHashHistory(),
   routes: [
     {
+      path: '/',
+      component: () => import('@/layouts/default/index.vue'),
+      children:[
+        {
+          path: 'test',
+          component: () => import('@/components/TestComponnet/index.vue'),
+        }
+      ]
+    },
+    {
       path: '/test',
       component: () => import('@comps/TestComponnet/index.vue')
     },
-    {
-      path: '/home',
-      component: () => import('@views/Home/index.vue')
-    }
   ]
 })
 export default router
