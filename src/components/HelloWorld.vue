6536ceea24a9717be3669834518fcd0366faffb7
hwb <313200064@qq.com>
2022-08-11 21:44:43 +0800

    feat: 增加环境变量

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -15,7 +15,7 @@ const count = ref(0);
 
 <template>
   <h1>{{ msg }}</h1>
-  <p @click="add">{{ $store.state.counter }}</p>
+  <p @click="add">{{ store.state.counter }}</p>
   <p>
     Recommended IDE setup:
     <a href="https://code.visualstudio.com/" target="_blank">VSCode</a>
